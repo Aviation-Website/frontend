0d4c4ed77e3dbef12159af2f1116e1df9ac2d270
Galal Elsayed <agalal3516@gmail.com>
2025-11-26 13:54:46 +0200

    Comment out Preloader import in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,7 @@ import { PilotPhrases } from "@/components/Home/PilotPhrases";
 import { StepsSection } from "@/components/Home/StepsSection/StepsSection";
 import { TrainingFeatures } from "@/components/Home/TrainingFeatures";
 import { Footer } from "@/components/Footer/Footer";
-import Preloader from "@/components/ui/Preloader";
+// import Preloader from "@/components/ui/Preloader";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
